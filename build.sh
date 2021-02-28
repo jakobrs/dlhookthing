7e2d5f196c7087fbe0e465f4e76c068709ea766d
jakobrs <jakobrs100@gmail.com>
2021-02-28 13:00:32 +0100

    Change generated dynamic library name to dlhook.so

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rustc ./dlhook.rs --crate-type cdylib
+rustc ./dlhook.rs --crate-type cdylib -o dlhook.so
 rustc ./dlhook.rs -C llvm-args=-x86-asm-syntax=intel --crate-type cdylib --emit asm
